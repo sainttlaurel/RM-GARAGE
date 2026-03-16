2190b6418b4f1cd4e0308e0638b7872385a8cf1e
Miguel Pilapil <84023613+sainttlaurel@users.noreply.github.com>
2026-03-16 17:02:17 +0800

    Delete PHP directory

--- PHP/footer.php
+++
@@ -1,5 +0,0 @@
-<!--FOOTER OF THE WEBPAGE-->
-<footer>
-  <p><i class="fa fa-copyright"></i> Ravi Modular Cabinet EST. 2023 <a href="https://www.facebook.com/ravimodularcabinet"
-      target="_blank" id="profile-link" class="facebookicon" title="Visit Our Facebook Page">Visit Our Page</a></p>
-</footer>
